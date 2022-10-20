1cf8e18ccbc75886cbbed28e2fea910a621842b3
Andre Silva <academicworks.adf@gmail.com>
2022-10-20 16:18:02 -0300

    working with futures

--- hospital2/src/hospital2/aula6.clj
+++ hospital2/src/hospital2/aula6.clj
@@ -9,9 +9,17 @@
 ; será feito na colecao que tiver algum dado alterado
 ; e não no conteudo completo
 
+(defn cabe-na-fila? [fila]
+  (-> fila
+      count
+      (< 5)))
 
-(defn chega-em [fila pessoa]
-  (conj fila pessoa))
+
+(defn chega-em
+  [fila pessoa]
+  (if (cabe-na-fila? fila)
+    (conj fila pessoa)
+    (throw (ex-info "Fila já está cheia" {:tentando-adicionar pessoa}))))
 
 ; troca de referencia via ref-set.
 ; Esse precisa usar (deref ou @)
@@ -35,8 +43,39 @@
         ]
 
     (dosync
-      (chega-em! hospital "Guilherme"))
+      (chega-em! hospital "Guilherme")
+      (chega-em! hospital "André")
+      (chega-em! hospital "Felipe")
+      (chega-em! hospital "Santos")
+      (chega-em! hospital "Paranauê")
+      ;(chega-em! hospital "Lis")
+      )
     (pprint hospital)
+    ))
+
+;(simula-um-dia)
+
+(defn async-chega-em! [hospital pessoa]
+  (future
+    (Thread/sleep (rand 500))
+          (dosync
+            (println "Tentando o código sincronizado" pessoa)
+            (chega-em! hospital pessoa))))
+
+(defn simula-um-dia []
+  (let [hospital
+        {:espera       (ref h.model/fila-vazia)
+         :laboratorio1 (ref h.model/fila-vazia)
+         :laboratorio2 (ref h.model/fila-vazia)
+         :laboratorio3 (ref h.model/fila-vazia)}
+        ]
+
+    (def futures (mapv #(async-chega-em! hospital %) (range 10)))
+
+    (future
+      (dotimes [n 4]
+        (Thread/sleep 2000)
+        ))
     ))
 
 (simula-um-dia)
