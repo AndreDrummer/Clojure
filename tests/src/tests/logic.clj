066a1103019b89a64744266149fd34a81861ee51
AndréDrummer <academicworks.adf@gmail.com>
2023-01-10 18:13:30 -0300

    automated tests with schemas

--- tests/src/tests/logic.clj
+++ tests/src/tests/logic.clj
@@ -29,6 +29,68 @@
 (defn cabe-na-fila?
   [hospital departamento]
   (some-> hospital
-      departamento
-      count
-      (< 5)))
+          departamento
+          count
+          (< 5)))
+
+;(defn chega-em
+;  [hospital departamento pessoa]
+;  (if (cabe-na-fila? hospital departamento)
+;    (update hospital departamento conj pessoa)
+;    (throw (IllegalStateException. "Não cabe ninguém neste departamento."))))
+
+; nil ???
+;(defn chega-em
+;  [hospital departamento pessoa]
+;  (if (cabe-na-fila? hospital departamento)
+;    (update hospital departamento conj pessoa)))
+
+; exemplo para usar ex-data
+;(defn chega-em
+;  [hospital departamento pessoa]
+;  (if (cabe-na-fila? hospital departamento)
+;    (update hospital departamento conj pessoa)
+;    (throw (ex-info "Não cabe ninguém neste departamento." {:paciente pessoa, :tipo :impossivel-colocar-pessoa-na-fila}))))
+
+
+;(defn- tenta-colocar-na-fila
+;  [hospital departamento pessoa]
+;  (if (cabe-na-fila? hospital departamento)
+;    (update hospital departamento conj pessoa)))
+;
+;(defn chega-em
+;  [hospital departamento pessoa]
+;  (if-let [novo-hospital (tenta-colocar-na-fila hospital departamento pessoa)]
+;    {:hospital novo-hospital :resultado :sucesso}
+;    {:hospital hospital :resultado :impossivel-colocar-pessoa-na-fila}))
+
+; antes de fazer swap chega-em vai ter que tratar o resultado
+;(defn chega-em!
+;  [hospital departamento pessoa]
+;  (chega-em hospital departamento pessoa))
+
+; Código do curso anterior
+
+(defn chega-em
+  [hospital departamento pessoa]
+  (if (cabe-na-fila? hospital departamento)
+    (update hospital departamento conj pessoa)
+    (throw (ex-info "Não cabe ninguém neste departamento." {:paciente pessoa}))))
+
+(defn atende
+  [hospital departamento]
+  (update hospital departamento pop))
+
+(defn proxima
+  [hospital departamento]
+  "Retorna a próxima pessoa da fila"
+  (-> hospital departamento peek))
+
+(defn transfere [hospital de para]
+  "Transfere o próximo paciente da fila 'de' para a fila 'para'"
+  (let [pessoa (proxima hospital de)]
+    (-> hospital
+        (atende de)
+        (chega-em para pessoa))
+    )
+  )
--- tests/test/tests/logic_test.clj
+++ tests/test/tests/logic_test.clj
@@ -1,27 +1,113 @@
 (ns tests.logic-test
+  (:use clojure.pprint)
   (:require [clojure.test :refer :all]
-            [tests.logic :refer :all]))
+            [tests.logic :refer :all]
+            [tests.model :as h.model]))
 
 (deftest cabe-na-fila?-test
-  ; Boundary tests
-  ; Exatamente na borda e one off. -1, +1, <=, >=, =
 
-  ; borda do zero
-  (testing "Que cabe numa fila vazia"
-    (is (cabe-na-fila? {:espera []} :espera)))
+  (let [hospital-cheio {:espera [32 65 87 41 69]}]
+    ; Boundary tests
+    ; Exatamente na borda e one off. -1, +1, <=, >=, =
 
-  ; borda do limite
-  (testing "Que não cabe na fila quando a fila está cheia"
-    (is (not (cabe-na-fila? {:espera [1 2 3 4 5]} :espera))))
+    ; borda do zero
+    (testing "Que cabe numa fila vazia"
+      (is (cabe-na-fila? {:espera []} :espera)))
 
-  ; one off da borda do limite pra cima
-  (testing "Que não cabe na fila quando tem mais do que uma fila cheia"
-    (is (not (cabe-na-fila? {:espera [1 2 3 4 5 6]} :espera))))
+    ; borda do limite
+    (testing "Que não cabe na fila quando a fila está cheia"
+      (is (not (cabe-na-fila? hospital-cheio :espera))))
 
-  (testing "Que cabe na fila quando tem gente mais não está cheia"
-    (is (cabe-na-fila? {:espera [1 2 3 4]} :espera))
-    (is (cabe-na-fila? {:espera [1 2]} :espera)))
+    ; one off da borda do limite pra cima
+    (testing "Que não cabe na fila quando tem mais do que uma fila cheia"
+      (is (not (cabe-na-fila? {:espera [1 2 3 4 5 6]} :espera))))
 
-  (testing "Que... quando o departamento não existe"
-    (is (not (cabe-na-fila? {:espera [1 2 3 4]} :raio-x))))
+
+    ; dentro das bordas
+    (testing "Que cabe na fila quando tem gente mais não está cheia"
+      (is (cabe-na-fila? {:espera [1 2 3 4]} :espera))
+      (is (cabe-na-fila? {:espera [1 2]} :espera)))
+
+    (testing "Que não cabe quando o departamento não existe"
+      (is (not (cabe-na-fila? {:espera [1 2 3 4]} :raio-x)))))
+  )
+
+(deftest chega-em-test
+
+  (let [hospital-cheio {:espera [32 65 87 41 69]}]
+
+    (testing "Aceita pessoas enquanto cabem pessoas na fila"
+      ; Este teste é ruim pois ele testa apenas que copiamos o mesmo código da lógica aqui no teste também.
+      ; (is (= (update {:espera [1 2 3 4 5]} :espera conj 5)
+      ;       (chega-em {:espera [1 2 3 4 5]}, :espera, 5)))
+
+      (is (= {:espera [1, 2, 3, 4, 5]}
+             (chega-em {:espera [1, 2, 3, 4]}, :espera, 5))
+          )
+
+
+      ;Testar não sequencialmente, essa é uma boa prática.
+      (is (= {:espera [1 2 5]}
+             (chega-em {:espera [1 2]}, :espera, 5)))
+      )
+
+    ;(is (= {:hospital {:espera [1, 2, 3, 4, 5]} :resultado :sucesso}
+    ;       (chega-em {:espera [1, 2, 3, 4]}, :espera, 5))
+    ;    )
+    ;
+    ;
+    ;;Testar não sequencialmente, essa é uma boa prática.
+    ;(is (= {:hospital {:espera [1 2 5]} :resultado :sucesso}
+    ;       (chega-em {:espera [1 2]}, :espera, 5)))
+    ;)
+
+
+    (testing "Não aceita quand não cabe na fila"
+      ; Essa não é uma abordagem inteligente
+      (is (thrown? clojure.lang.ExceptionInfo (chega-em hospital-cheio, :espera 76)))
+
+      ; Essa aqui já é um pouco melhor, mas ainda assim não é a melhor opção.
+      ;(is (thrown? IllegalStateException (chega-em hospital-cheio, :espera 76))))
+
+      ;outra abordagem com nil, mas temos o perigo do swap, teremos que trabalhar em outro ponto.
+      ;(is (nil? (chega-em hospital-cheio :espera 76)))
+
+      ; outra maneira de testar, onde ao inves de como JAVA, utilizar o tipo de exception para entender
+      ; o TIPO de erro que ocorreu, estou usando os dados de exception para isso
+      ;
+      ;(is (try
+      ;      (chega-em hospital-cheio, :espera 85)
+      ;      (catch clojure.lang.ExceptionInfo e
+      ;        (= :impossivel-colocar-pessoa-na-fila (:tipo (ex-data e)))
+      ;        )))
+
+      ;(is (= {:hospital hospital-cheio :resultado :impossivel-colocar-pessoa-na-fila}
+      ;       (chega-em hospital-cheio, :espera 76)))
+
+      )
+    )
+  )
+
+
+(deftest transfere-test
+  (testing "Aceita pessoas se cabe"
+    (let [hospital-original {:espera [7] :raio-x []}]
+      (is (= {:espera []
+              :raio-x [7]}
+             (transfere hospital-original :espera :raio-x)))
+      )
+
+    (let [hospital-original {:espera (conj h.model/fila-vazia 51 5) :raio-x (conj h.model/fila-vazia 13)}]
+      (is (= {:espera [5]
+              :raio-x [13 51]}
+             (transfere hospital-original :espera :raio-x)))
+      )
+    )
+
+  (testing "Recusa pessoas se não cabe"
+    (let [hospital-cheio {:espera [7] :raio-x [9 4 6 7 5]}]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (transfere hospital-cheio :espera :raio-x)))
+      )
+    )
   )
